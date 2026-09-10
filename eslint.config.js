c7a42a76d9b885764a46e2c7f2dbdd43cc5b4748
Himavanth Kumar <himavanthkumar005@gmail.com>
2026-09-10 13:52:21 +0530

    Optimize configuration for Render deployment: zero-warning lint and quoted Node version

--- eslint.config.js
+++ eslint.config.js
@@ -18,10 +18,7 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
     rules: {
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': 'off',
     },
   },
 ])
